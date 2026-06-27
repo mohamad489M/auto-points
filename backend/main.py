6f95fc581e8ea6e743aec56d6f559801be4decf2
mohamad489M <mhmhhh4114@gmail.com>
2026-06-27 23:18:53 +0300

    Fix port touse Railway PORT env

--- backend/main.py
+++ backend/main.py
@@ -322,4 +322,6 @@ async def startup_event():
 
 if __name__ == "_main_":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+port = int(os.environ.get("PORT", 8000))
+uvicorn.run(app, host="0.0.0.0", port=port)
